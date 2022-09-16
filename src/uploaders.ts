1babe2a0410c2fa1b8eb6181dafd45dcf12cf49f
Aleksey Elyseev <aelyseev@gmail.com>
2022-09-16 12:34:44 +0400

    feat: Allow custom rewrites

--- src/uploaders.ts
+++ src/uploaders.ts
@@ -22,106 +22,111 @@ const appTmpl = ejs.compile(fs.readFileSync(path.join(dirname, './tmpl', 'app.ej
 const swTmpl = ejs.compile(fs.readFileSync(path.join(dirname, './tmpl', 'sw.ejs'), 'utf-8'));
 
 const findFiles = (folder: string): string[] => {
-    if (!path.isAbsolute(folder)) {
-        throw Error(`An absolute path should be provided, ${folder} provided`);
+  if (!path.isAbsolute(folder)) {
+    throw Error(`An absolute path should be provided, ${folder} provided`);
+  }
+  const entries = fs.readdirSync(folder);
+  const files: string[] = [];
+  entries.forEach(entry => {
+    const fullPath = path.join(folder, entry);
+    const stats = fs.statSync(fullPath);
+    if (stats.isFile()) {
+      files.push(fullPath)
+    } else if (stats.isDirectory()) {
+      // eslint-disable-next-line prefer-spread
+      files.push.apply(files, findFiles(fullPath));
     }
-    const entries = fs.readdirSync(folder);
-    const files: string[] = [];
-    entries.forEach(entry => {
-        const fullPath = path.join(folder, entry);
-        const stats = fs.statSync(fullPath);
-        if (stats.isFile()) {
-            files.push(fullPath)
-        } else if (stats.isDirectory()) {
-          // eslint-disable-next-line prefer-spread
-            files.push.apply(files, findFiles(fullPath));
-        }
-    });
-    return files;
+  });
+  return files;
 };
 
 const buildFolderVersion = (folder: string, salt = '') => {
-    const files = findFiles(folder);
-    files.sort();
-    return files
-        .reduce(
-            (hash, file) => createHash('sha1').update(`${hash}${fs.readFileSync(file).toString().trim()}`).digest('hex'),
-            salt
-        );
+  const files = findFiles(folder);
+  files.sort();
+  return files
+    .reduce(
+      (hash, file) => createHash('sha1').update(`${hash}${fs.readFileSync(file).toString().trim()}`).digest('hex'),
+      salt
+    );
 };
 
-const buildDdcFileUrl = async (ddcClient: DdcClient, bucketId: bigint, appId: string, filePath: string = '.'): Promise<string> => {
-    const url = new URL(ddcClient.caStorage.cdnNodeUrl);
-    url.pathname = path.join(`/ddc/buc/${bucketId}/file`, appId, filePath);
-    return url.href;
+const buildDdcFileUrl = async (ddcClient: DdcClient, bucketId: bigint, appId: string,
+  filePath: string = '.'): Promise<string> => {
+  const url = new URL(ddcClient.caStorage.cdnNodeUrl);
+  url.pathname = path.join(`/ddc/buc/${bucketId}/file`, appId, filePath);
+  return url.href;
 }
 
 type UploadAppOptions = {
-    folder: string;
-    index?: string;
-    appId?: string;
-    bucketId: bigint;
+  folder: string;
+  index?: string;
+  appId?: string;
+  bucketId: bigint;
+  rewrites: Record<string | 'sw.js', string>;
 };
 
-export const uploadWebApp = async (ddcClient: DdcClient, { folder, index, appId, bucketId }: UploadAppOptions): Promise<{ appId: string; url: string }> => {
-    const applicationPath = appId || nanoid();
-    const files = findFiles(folder);
-    const indexFilePath = path.join(folder, index || 'index.html');
-    if (!fs.existsSync(indexFilePath)) {
-        throw Error(`Index file ${indexFilePath} not found`);
+export const uploadWebApp = async (ddcClient: DdcClient,
+  { folder, index, appId, bucketId, rewrites }: UploadAppOptions): Promise<{ appId: string; url: string }> => {
+  const applicationPath = appId || nanoid();
+  const files = findFiles(folder);
+  let indexFileName = index || 'index.html';
+  const indexFilePath = path.join(folder, indexFileName);
+  if (!fs.existsSync(indexFilePath)) {
+    throw Error(`Index file ${indexFilePath} not found`);
+  }
+  const routes: Record<string, string> = {};
+  let mainRoute = '';
+  let count = 1;
+  await Promise.all(files.map(async (file) => {
+    const relativePath = path.relative(folder, file);
+    const fileRoute = file === indexFilePath ? undefined : path.join(applicationPath, relativePath);
+    const response = await storeFile(ddcClient, {
+      filename: file,
+      bucketId,
+      path: fileRoute,
+    });
+    if (file === indexFilePath) {
+      mainRoute = rewrites[indexFileName] || response.url;
+    } else {
+      const rewritePath = rewrites[relativePath] || relativePath
+      routes[relativePath] = await buildDdcFileUrl(ddcClient, bucketId, applicationPath, rewritePath);
     }
-    const routes: Record<string, string> = {};
-    let mainRoute = '';
-    let count = 1;
-    await Promise.all(files.map(async (file) => {
-        const relativePath = path.relative(folder, file);
-        const fileRoute = file === indexFilePath ? undefined : path.join(applicationPath, relativePath);
-        const response = await storeFile(ddcClient, {
-            filename: file,
-            bucketId,
-            path: fileRoute,
-        });
-        if (file === indexFilePath) {
-            mainRoute = response.url;
-        } else {
-            routes[relativePath] = await buildDdcFileUrl(ddcClient, bucketId, applicationPath, relativePath);
-        }
-        console.log(`${count}/${files.length}: ${file}`);
-        count += 1;
-    }))
+    console.log(`${count}/${files.length}: ${file}`);
+    count += 1;
+  }))
 
-    const version = buildFolderVersion(folder);
-    const swJs = swTmpl({ version });
-    const swAppId = `${applicationPath}-sw.js`;
-    const swTags = [
-        new Tag('timestamp', new Uint8Array(encode(Date.now()))),
-        new Tag('file-path', swAppId),
-        new Tag('content-type', 'application/javascript', SearchType.NOT_SEARCHABLE)
-    ];
-    const swFile = new DdcFile(Buffer.from(swJs, 'utf-8'), swTags);
-    await ddcClient.store(bucketId, swFile, {
-        encrypt: false,
-    });
-    const swRoute = await buildDdcFileUrl(ddcClient, bucketId, swAppId);
+  const version = buildFolderVersion(folder);
+  const swJs = swTmpl({ version });
+  const swAppId = rewrites['sw.js'] ? path.join(applicationPath, rewrites['sw.js']) : `${applicationPath}-sw.js`;
+  const swTags = [
+    new Tag('timestamp', new Uint8Array(encode(Date.now()))),
+    new Tag('file-path', swAppId),
+    new Tag('content-type', 'application/javascript', SearchType.NOT_SEARCHABLE)
+  ];
+  const swFile = new DdcFile(Buffer.from(swJs, 'utf-8'), swTags);
+  await ddcClient.store(bucketId, swFile, {
+    encrypt: false,
+  });
+  const swRoute = await buildDdcFileUrl(ddcClient, bucketId, swAppId);
 
-    const loaderHtml = appTmpl({
-        sw: swRoute,
-        mainRoute,
-        routes,
-    });
+  const loaderHtml = appTmpl({
+    sw: rewrites['sw.js'] ? rewrites['sw.js'] : swRoute,
+    mainRoute,
+    routes,
+  });
 
-    const loaderTags = [
-        new Tag('timestamp', new Uint8Array(encode(Date.now()))),
-        new Tag('file-path', applicationPath),
-        new Tag('content-type', 'text/html', SearchType.NOT_SEARCHABLE)
-    ];
-    const loaderFile = new DdcFile(Buffer.from(loaderHtml, 'utf-8'), loaderTags);
-    await ddcClient.store(bucketId, loaderFile, {
-        encrypt: false,
-    });
+  const loaderTags = [
+    new Tag('timestamp', new Uint8Array(encode(Date.now()))),
+    new Tag('file-path', applicationPath),
+    new Tag('content-type', 'text/html', SearchType.NOT_SEARCHABLE)
+  ];
+  const loaderFile = new DdcFile(Buffer.from(loaderHtml, 'utf-8'), loaderTags);
+  await ddcClient.store(bucketId, loaderFile, {
+    encrypt: false,
+  });
 
-    return {
-        url: await buildDdcFileUrl(ddcClient, bucketId, applicationPath),
-        appId: applicationPath,
-    }
+  return {
+    url: await buildDdcFileUrl(ddcClient, bucketId, applicationPath),
+    appId: applicationPath,
+  }
 }
